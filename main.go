c7320a4f0fadb5f484206d3f89cb1736353ab1cf
vroxo <vitorroxo.dev@gmail.com>
2022-09-15 11:06:13 -0300

    feat: argo

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello word FC!"))
+		w.Write([]byte("Hello Argo!"))
 	})
 
 	http.ListenAndServe(":8080", nil)
